e43889e74c6824c917354c194f982452c7e6605c
Fitria Fadila <fitriafadhilah22@gmail.com>
2024-09-15 09:52:52 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const loginForm = document.getElementById("loginForm");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  if (email === "" || password === "") {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  if (email === "rosynately@gmail.com" && password === "123456") {
+    alert("Login successful!");
+
+    window.location.href = "home.html";
+  } else {
+    alert("Invalid email or password. Please try again.");
+  }
+});
